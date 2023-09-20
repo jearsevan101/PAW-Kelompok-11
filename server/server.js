d698e1a0f1301e4a8af82924af7781f1296323b7
afixv <afixv00@gmail.com>
2023-09-20 21:40:18 +0700

    feat: use 'Kendaraan' router in server.js

--- server/server.js
+++ server/server.js
@@ -6,4 +6,7 @@ app.use("/", (req, res, next) => {
     res.send("Hello World");
 }); 
 
+const KendaraanRouter = require('./routers/kendaraan.router');
+app.use('/kendaraan', KendaraanRouter);
+
 app.listen(5000);
