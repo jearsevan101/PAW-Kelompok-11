96e787b2431bdf0c567526656dd2e4b9f51d4c92
dundorma <90293691+dundorma@users.noreply.github.com>
2023-09-21 22:22:12 +0700

    remove old backend directory

--- server/routers/kendaraan.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const Kendaraan = require("../controllers/kendaraan.controller");
-
-// Update a vehicle by ID
-router.put("/:id", Kendaraan.update);
-
-module.exports = router;
