a434618f65bceb44f0f67b4b8b2ec0ddb5e3867f
afixv <afixv00@gmail.com>
2023-09-22 00:52:51 +0700

    feat: Add PUT API route for updating kendaraan

--- backend/routes/kendaraan.routes.js
+++ backend/routes/kendaraan.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { createKendaraan } = require("../controllers/kendaraan.controller");
+const { createKendaraan, updateKendaraan } = require("../controllers/kendaraan.controller");
 
 const router = express.Router();
 
 // POST API
 router.post("/", createKendaraan);
+
+//PUT API
+router.put("/:id", updateKendaraan);
 
 module.exports = router;
