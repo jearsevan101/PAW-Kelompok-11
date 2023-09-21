95901d032e28170ad6c9875255959e4af133bec8
afixv <afixv00@gmail.com>
2023-09-22 00:53:37 +0700

    feat: Add kendaraan update

--- backend/controllers/kendaraan.controller.js
+++ backend/controllers/kendaraan.controller.js
@@ -20,6 +20,36 @@ const createKendaraan = async (req, res) => {
   }
 };
 
+// Update kendaraan
+const updateKendaraan = async (req, res) => {
+  const { id } = req.params; 
+  const { nama, deskripsi, lokasi, harga, available, img_url } = req.body;
+
+  try {
+    const kendaraan = await Kendaraan.findByIdAndUpdate(
+      id,
+      {
+        nama,
+        deskripsi,
+        lokasi,
+        harga,
+        available,
+        img_url,
+      },
+      { new: true } 
+    );
+
+    if (!kendaraan) {
+      return res.status(404).json({ error: "Kendaraan tidak ditemukan" });
+    }
+
+    res.status(200).json(kendaraan);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+
 module.exports = {
-  createKendaraan,
+  createKendaraan, updateKendaraan
 };
