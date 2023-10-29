82904d85e00ebfd66588699ee44505632ff3308b
afixv <afixv00@gmail.com>
2023-10-29 22:19:05 +0700

    feat: setup google fonts

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>RentalIn</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
