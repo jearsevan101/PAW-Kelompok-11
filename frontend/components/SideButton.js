e5c564ed0aa5d59227e1dcbc9e8b6de14a8ce516
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2023-11-29 01:00:32 +0700

    feat: update sidebutton

--- frontend/components/SideButton.js
+++ frontend/components/SideButton.js
@@ -1,7 +1,7 @@
 const SideButton = ({ children, selected, icon, ...props }) => {
     const buttonStyle = selected 
-        ? "bg-c-primary flex flex-row items-center text-white w-48 h-10 font-regular px-5 py-2 rounded-md hover:opacity-90 focus:outline-none focus:ring text-left" // styles when selected
-        : "bg-c-light-grey flex flex-row items-center text-c-text-grey w-48 h-10 font-regular px-5 py-2 rounded-md hover:opacity-90 focus:outline-none focus:ring text-left"; // styles when not selected
+        ? "bg-c-primary flex flex-row items-center text-white w-full sm:w-48 h-10 font-regular px-5 py-2 rounded-md hover:opacity-90 focus:outline-none focus:ring text-left text-lg sm:text-base" // styles when selected
+        : "bg-c-light-grey flex flex-row items-center text-c-text-grey w-full sm:w-48 h-10 font-regular px-5 py-2 rounded-md hover:opacity-90 focus:outline-none focus:ring text-left text-lg sm:text-base"; // styles when not selected
 
     return (
         <button className={` ${buttonStyle}`} {...props}>
