f86c77d57ae303faeee72e9fe0bafc324df49f1f
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2023-12-03 21:00:49 +0700

    feat: responsivity for side bar

--- frontend/components/SideBar.js
+++ frontend/components/SideBar.js
@@ -1,87 +1,30 @@
-import { FaCar, FaList, FaTachometerAlt } from "react-icons/fa";
-import { useState, useEffect } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-
-const SideButton = ({ children, selected, icon, ...props }) => {
-  return (
-    <button
-      className={`${
-        selected
-          ? "bg-c-primary flex flex-row items-center text-white w-full sm:w-48 h-10 font-regular px-5 py-2 rounded-md hover:opacity-90 focus:outline-none focus:ring text-left text-lg sm:text-base"
-          : "bg-c-light-grey flex flex-row items-center text-c-text-grey w-full sm:w-48 h-10 font-regular px-5 py-2 rounded-md hover:opacity-90 focus:outline-none focus:ring text-left text-lg sm:text-base"
-      }`}
-      {...props}
-      aria-current={selected ? "page" : undefined}>
-      {icon}
-      <span className="ml-3">{children}</span>
-    </button>
-  );
-};
+import { FaCar, FaList, FaTachometerAlt } from 'react-icons/fa';
+import { useState } from 'react';
+import Link from 'next/link';
+import SideButton from './SideButton';
 
 const SideBar = () => {
-  const [selectedButton, setSelectedButton] = useState("dashboard");
+  const [selectedButton, setSelectedButton] = useState('dashboard');
   const icons = {
-    dashboard: <FaTachometerAlt />,
-    "add-car": <FaCar />,
-    "list-car": <FaList />,
+      'dashboard': <FaTachometerAlt />,
+      'add-car': <FaCar />,
+      'list-car': <FaList />
   };
-
-  const menuItems = [
-    {
-      id: "dashboard",
-      label: "Dashboard",
-      link: "./dashboard",
-      actualLink: "/admin/dashboard",
-    },
-    {
-      id: "add-car",
-      label: "Add Car",
-      link: "./add-car",
-      actualLink: "/admin/add-car",
-    },
-    {
-      id: "list-car",
-      label: "Car List",
-      link: "./list-car",
-      actualLink: "/admin/list-car",
-    },
-  ];
-
-  const router = useRouter();
-
-  useEffect(() => {
-    const currentRoute = router.asPath;
-    const selectedItem = menuItems.find(
-      (item) => currentRoute === item.actualLink
-    );
-    console.log("Selected Item:", selectedItem);
-    if (selectedItem) {
-      setSelectedButton(selectedItem.id);
-    }
-  }, [router.asPath]);
-
   return (
-    <div className="fixed top-0 left-0 bg-primary-0 w-64 h-full text-justify text-lightsteelblue-100 flex flex-col p-4 overflow-hidden mt-[96px]">
+    <div className="object-cover relative w-full sm:w-[250px] h-[screen] overflow-hidden bg-[#FFFFFF]">
       <h2 className="font-semibold text-c-text-grey text-[12px] px-5 tracking-widest mb-4">
-        MAIN MENU
+            MAIN MENU
       </h2>
       <div className="flex flex-col items-start justify-start gap-[10px] text-base text-secondary-300 mt-5">
-        {menuItems.map((item) => (
-          <Link key={item.id} href={item.link}>
-            <SideButton
-              icon={icons[item.id]}
-              selected={selectedButton === item.id}
-              onClick={() => setSelectedButton(item.id)}>
-              {item.label}
-            </SideButton>
-          </Link>
-        ))}
-      </div>
-      <div className="mt-auto mb-4 flex flex-row items-center justify-start gap-[12px] text-base text-secondary-300">
-        <div className="relative tracking-[-0.02em] leading-[150%] font-medium flex items-center w-[68px] h-7 shrink-0">
-          Log Out
-        </div>
+        <Link href="./dashboard">
+            <SideButton icon={icons['dashboard']} selected={selectedButton === 'dashboard'} onClick={() => setSelectedButton('dashboard')}>Dashboard</SideButton>
+        </Link>
+        <Link href="./add-car">
+          <SideButton icon={icons['add-car']} selected={selectedButton === 'add-car'} onClick={() => setSelectedButton('add-car')}>Add Car</SideButton>
+        </Link>
+        <Link href="./list-car">
+          <SideButton icon={icons['list-car']} selected={selectedButton === 'list-car'} onClick={() => setSelectedButton('list-car')}>Car List</SideButton>
+        </Link>
       </div>
     </div>
   );
